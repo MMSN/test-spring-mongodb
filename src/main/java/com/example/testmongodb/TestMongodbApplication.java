25ca0dd2a6a4794ef82bba3e280660a625bad64c
Mateus Nascimento <mateus.msnascimento@gmail.com>
2022-12-04 20:19:18 -0300

    chore: begin project

---
+++ src/main/java/com/example/testmongodb/TestMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.testmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMongodbApplication.class, args);
+	}
+
+}
